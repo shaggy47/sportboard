fb77dc3f3d44a85e232543d76ca173d8b553fcc3
vaibhavpfatkal <vaibhavpfatkal@gmail.com>
2018-02-10 16:00:22 +0530

    Hello express added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(request, response){
+    response.send("Hello World");
+});
+
+app.listen(3000,function(){
+    console.log("express started listening on port 3000");
+});
