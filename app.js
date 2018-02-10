a3083bffd775a0e030ee1c22b90592e0b302440a
vaibhavpfatkal <vaibhavpfatkal@gmail.com>
2018-02-10 20:44:02 +0530

    Documentation, geohelper added:

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-var express = require('express');
-
-var app = express();
-
-app.get('/', function(request, response){
-    response.send("Hello World");
-});
-
-app.listen(3000,function(){
-    console.log("express started listening on port 3000");
-});
+var geoTracker = require('./lib/geotrackHelper');
+var distance = geoTracker.getDistance(18.609486,73.77138, 18.56248, 73.78382);
+console.log(distance);
