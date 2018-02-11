d9a7d47f21f33474d4c5503d74e66b35b7ee0e6d
vaibhavpfatkal <vaibhavpfatkal@gmail.com>
2018-02-11 20:36:35 +0530

    for simplicity, switched to haversine distance

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("We will go there");
+var trackHelper = require('./lib/geotrackHelper');
+
+var emiter = trackHelper.emiter();
+emiter.on('data-loaded', function(data){
+    var distance = trackHelper.getTotalDistance(data);
+    console.log(distance);
+});
+trackHelper.loadFile('./Documentation/MyRide.gpx',emiter);
+
+
+
+
